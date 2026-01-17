49b2a45bafaae6dd39f5aa3d6808091dd099237f
Joel Chen <joel123@gmail.com>
2026-01-16 19:05:21 -0800

    SSH-370: Add desh shell wrapper and improve permission prompt

--- src/cli/desh.ts
+++ src/cli/desh.ts
@@ -1,4 +1,4 @@
-#!/usr/bin/env -S deno run --allow-read --allow-write --allow-run --allow-env
+#!/usr/bin/env -S deno run --allow-read --allow-write --allow-run --allow-env --config=/Users/jc/dev/safesh/deno.json
 /**
  * desh - Deno Shell CLI for SafeShell
  *
