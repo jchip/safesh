6140dffd71b7e951516fed5c00353c594649d37c
Joel Chen <joel123@gmail.com>
2026-02-08 19:53:10 -0800

    SSH-523: Remove dead code from streams/mod.ts

--- src/streams/mod.ts
+++ src/streams/mod.ts
@@ -1,33 +1,8 @@
 /**
  * Streaming API
  *
- * Gulp-like streaming primitives using Deno's native streams.
+ * Placeholder module for future gulp-like streaming primitives using Deno's native streams.
+ * See SSH-23, SSH-24, SSH-25 for planned implementation.
  *
  * @module
  */
-
-// TODO: Implement after SSH-23, SSH-24, SSH-25
-
-export function src(_pattern: string): ReadableStream<string> {
-  // TODO: Implement glob-based file source
-  throw new Error("Not implemented");
-}
-
-export function dest(_path: string): WritableStream<string> {
-  // TODO: Implement file destination
-  throw new Error("Not implemented");
-}
-
-export function transform<T, U>(
-  _fn: (chunk: T) => U | Promise<U>,
-): TransformStream<T, U> {
-  // TODO: Implement transform
-  throw new Error("Not implemented");
-}
-
-export function filter<T>(
-  _predicate: (chunk: T) => boolean | Promise<boolean>,
-): TransformStream<T, T> {
-  // TODO: Implement filter
-  throw new Error("Not implemented");
-}
